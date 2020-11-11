9e068bb472debe8f5caa6c77eb4c3b4948431349
ra sta <raminta.stanulionyte@gmail.com>
2020-11-12 00:00:49 +0100

    Android App Finished

---
+++ TipCalculator.Android/Views/SplashScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Views;
+using MvvmCross.Platforms.Android.Core;
+using Android.Content.PM;
+
+namespace TipCalculator.Android
+{
+    [Activity(
+    Label = "@string/app_name",
+    MainLauncher = true,
+    Icon = "@drawable/icon",
+    Theme = "@style/Theme.Splash",
+    NoHistory = true,
+    ScreenOrientation = ScreenOrientation.Portrait)]
+
+    public class SplashScreen : MvxSplashScreenActivity<MvxAndroidSetup<Core.App>, Core.App>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
